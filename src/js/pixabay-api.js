074dca87a3e9e3f03b15d2767cfa167d156d9121
Dmytro Serhiienko <conneciones@gmail.com>
2025-12-23 21:19:40 +0200

    work

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '53639618-7ccf198a06ad48a4b9e98f6f9';
+
+export async function fetchImages(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  const response = await axios.get(BASE_URL, { params });
+
+  return response.data;
+}
